47edf955c56ccfe5c5b5546e7d253d1dd2e1cb1c
LalithSrinivas <srinivaskaladi3096@gmail.com>
2020-06-12 09:43:45 +0530

    removed a few comments

--- src/main/scala/com/audienceproject/spark/dynamodb/datasource/DynamoBatchWriter.scala
+++ src/main/scala/com/audienceproject/spark/dynamodb/datasource/DynamoBatchWriter.scala
@@ -36,7 +36,6 @@ class DynamoBatchWriter(batchSize: Int,
 
     protected val buffer: ArrayBuffer[InternalRow] = new ArrayBuffer[InternalRow](batchSize)
     protected val rateLimiter: RateLimiter = RateLimiter.create(connector.writeLimit)
-//    println("(DynamoBatchWriter) writeLimit= " + connector.writeLimit)
     override def write(record: InternalRow): Unit = {
         buffer += record.copy()
         println(record.copy())
